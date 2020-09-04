0471a5220c5d0a94c22568756fe9e8b816efe8ac
jefk <speedracer111@gmail.com>
2020-09-03 21:14:42 -0700

    add scores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import scores from "../data/scores";
 
 const Home = () => (
   <>
@@ -7,10 +8,31 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <main>
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+    <main className="container mx-auto">
+      <div className="md:px-32 py-8 w-full">
+        <div className="shadow overflow-hidden rounded border-b border-gray-200">
+          <table className="min-w-full bg-white">
+            <thead className="bg-gray-800 text-white">
+              <tr>
+                <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                  Name
+                </th>
+                <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                  Score
+                </th>
+              </tr>
+            </thead>
+            <tbody className="text-gray-700">
+              {scores.map(([name, score]) => (
+                <tr>
+                  <td className="py-3 px-4">{name}</td>
+                  <td className="py-3 px-4">{score.toLocaleString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
 
     <footer>
